57add03f5962141f8ff3cbeed393d1b2617e2737
Stricted <info@stricted.net>
2018-09-22 00:22:31 +0200

    exynos9810-common: copy media codec files
    Change-Id: I0fe4f491037be7a168e29d8f4ab58ad817b6de4b

--- common.mk
+++ common.mk
@@ -157,6 +157,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libepicoperator
 
+# Media
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+
 # Memtrack
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl:64 \
