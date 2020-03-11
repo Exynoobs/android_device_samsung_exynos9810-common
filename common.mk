69585035eb477e7e0ab911d2bb1bada48561a0f9
Andreas Schneider <asn@cryptomilk.org>
2020-03-11 20:37:29 +0100

    exynos9810-common: build lineage livedisplay HAL
    * Also set permissions for mdnie lux node in init.exynos9810.rc
    
    Change-Id: I11dfc1df9eb4bd30c311d7b65ae6b4d1c59f54cb

--- common.mk
+++ common.mk
@@ -127,6 +127,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.samsung
 
+# Livedisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service.samsung-exynos
+
 # Memtrack
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl:64 \
