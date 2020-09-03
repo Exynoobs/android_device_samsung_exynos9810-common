cdbddfb339170e6357e350daa6ec275bcc07f98e
Andreas Schneider <asn@cryptomilk.org>
2020-09-03 15:02:31 +0200

    shims: add libshim_sensorndkbridge from exynos9820-common
    Change-Id: I9f964211782f3f9945d5791ddf19a2b83626cc49

--- common.mk
+++ common.mk
@@ -187,6 +187,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Shims
+PRODUCT_PACKAGES += \
+    libshim_sensorndkbridge
+
 # Soundtrigger
 PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.0-impl:32
