e552a98ffc26bbe165c9f8b617aa57fd1607ee7e
Tim Zimmermann <tim@linux4.de>
2020-12-18 10:51:06 +0100

    exynos9810-common: add nfc config
    Change-Id: I50d0b88aa5fde272a4923c0c5014e5181f18eeb6

--- platform/config.mk
+++ platform/config.mk
@@ -17,6 +17,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/gps/gps_psds.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/gps/gps_psds.conf
 
+### NFC
+PRODUCT_COPY_FILES += \
+    $(PLATFORM_PATH)/config/nfc/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf
+
 ### PUBLIC LIBRARIES
 PRODUCT_COPY_FILES += \
     $(PLATFORM_PATH)/config/linker/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
