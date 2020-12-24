e607658c79b6021db038ebf47f4a1058d60d503e
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-12-24 16:18:14 +0100

    exynos9810-common: Build fastcharge HIDL
    Change-Id: Iec1a7faec24f0d5b4b1d2f1ef52b781368c5bd51

--- common.mk
+++ common.mk
@@ -86,6 +86,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
+# FastCharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.samsung
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.samsung
