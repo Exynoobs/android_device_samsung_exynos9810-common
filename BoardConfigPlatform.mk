5d7b2ebd7fb6dd0c53b6ea03138db191bfac3ecc
Tim Zimmermann <tim@linux4.de>
2021-04-22 21:36:41 +0200

    exynos9810-common: bluetooth: build libbt-vendor from source
    Change-Id: Id2c01752b590f46a4f91cae14f96b29811bc3baf

--- BoardConfigPlatform.mk
+++ BoardConfigPlatform.mk
@@ -111,6 +111,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 ### BLUETOOTH
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(PLATFORM_PATH)/hardware/bluetooth
 BOARD_CUSTOM_BT_CONFIG := $(PLATFORM_PATH)/hardware/bluetooth/libbt_vndcfg.txt
+BOARD_HAVE_BLUETOOTH_BCM := true
 
 ### GRAPHICS
 # hardware/interfaces/configstore/1.1/default/surfaceflinger.mk
