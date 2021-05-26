a8b8337b2db5abfb7748a0117ad4398fa55bbfbd
Steven Moreland <smoreland@google.com>
2021-05-26 00:41:38 +0000

    exynos9810-common: Move /vendor/ueventd.rc to /vendor/etc/ueventd.rc
    The root location of this file is deprecated for Android T.
    
    Bug: 189268918
    Bug: 178221726
    Test: device reaches boot complete after raising API level to 32
    Change-Id: I97a6d2e5cacd0ac5eeb81daafeb8b1a0ad5c119b

--- common.mk
+++ common.mk
@@ -133,7 +133,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init/init.samsungexynos9810.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.samsungexynos9810.rc \
     $(COMMON_PATH)/configs/init/init.samsungexynos9810.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.samsungexynos9810.usb.rc \
     $(COMMON_PATH)/configs/init/init.samsung.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.samsung.rc \
-    $(COMMON_PATH)/configs/init/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
+    $(COMMON_PATH)/configs/init/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc \
     $(COMMON_PATH)/configs/init/init.baseband.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.baseband.rc
 
 # Keylayout
