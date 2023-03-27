bf50183ff8e0da9f05ec75ca3f5f79ae9981ed81
Gabriel Biren <gbiren@google.com>
2023-03-27 22:33:41 +0000

    universal2100-common: Update makefile to use the AIDL Wifi Vendor HAL.
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1

--- common.mk
+++ common.mk
@@ -322,7 +322,7 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     hostapd \
     WifiOverlay \
     wpa_supplicant \
